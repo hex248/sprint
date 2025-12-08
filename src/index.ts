8e76b0ce3837c322d6727cd4689d2895c3796fa8
Oliver Bryan <04oliverbryan@gmail.com>
2025-12-08 00:16:20 +0000

    moved demo data creation to util file

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
 import { db, testDB } from "./db/client";
 import { User } from "./db/schema";
 import { routes } from "./routes";
-import { createUser, createIssue, createProject } from "./db/queries";
+import { createDemoData } from "./utils";
 
 const DEV = process.argv.find((arg) => ["--dev", "--developer", "-d"].includes(arg.toLowerCase())) != null;
 const PORT = process.argv.find((arg) => arg.toLowerCase().startsWith("--port="))?.split("=")[1] || 0;
-
-const createDemoData = async () => {
-    const user = await createUser("Demo User", "demo_user");
-    if (!user) {
-        throw new Error("failed to create demo user");
-    }
-
-    const projectNames = ["PROJ", "TEST", "SAMPLE"];
-    for (const name of projectNames) {
-        const project = await createProject(name.slice(0, 4), name, user);
-
-        for (let i = 1; i <= 5; i++) {
-            await createIssue(
-                project.id,
-                `Issue ${i} in ${name}`,
-                `This is a description for issue ${i} in ${name}.`,
-            );
-        }
-    }
-};
 
 const main = async () => {
     const server = Bun.serve({
---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+import { createIssue, createProject, createUser } from "./db/queries";
+
+export const createDemoData = async () => {
+    const user = await createUser("Demo User", "demo_user");
+    if (!user) {
+        throw new Error("failed to create demo user");
+    }
+
+    const projectNames = ["PROJ", "TEST", "SAMPLE"];
+    for (const name of projectNames) {
+        const project = await createProject(name.slice(0, 4), name, user);
+
+        for (let i = 1; i <= 5; i++) {
+            await createIssue(
+                project.id,
+                `Issue ${i} in ${name}`,
+                `This is a description for issue ${i} in ${name}.`,
+            );
+        }
+    }
+};
