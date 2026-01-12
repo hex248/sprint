5e8a2eeff8ee5857c9c2c12cd237a300ed015aa7
Oliver Bryan <04oliverbryan@gmail.com>
2026-01-12 01:05:37 +0000

    added sprint db queries

--- packages/backend/src/db/queries/index.ts
+++ packages/backend/src/db/queries/index.ts
@@ -2,5 +2,6 @@ export * from "./issues";
 export * from "./organisations";
 export * from "./projects";
 export * from "./sessions";
+export * from "./sprints";
 export * from "./timed-sessions";
 export * from "./users";
---
+++ packages/backend/src/db/queries/sprints.ts
@@ -0,0 +1,27 @@
+import { Sprint } from "@issue/shared";
+import { eq } from "drizzle-orm";
+import { db } from "../client";
+
+export async function createSprint(
+    projectId: number,
+    name: string,
+    color: string | undefined,
+    startDate: Date,
+    endDate: Date,
+) {
+    const [sprint] = await db
+        .insert(Sprint)
+        .values({
+            projectId,
+            name,
+            ...(color ? { color } : {}),
+            startDate,
+            endDate,
+        })
+        .returning();
+    return sprint;
+}
+
+export async function getSprintsByProject(projectId: number) {
+    return await db.select().from(Sprint).where(eq(Sprint.projectId, projectId));
+}
